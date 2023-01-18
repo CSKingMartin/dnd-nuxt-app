676503bf2e87ca3f13eda9286681d4c0be701411
Conner P <conner.c.wells@gmail.com>
2023-01-18 12:42:28 -0800

    commits tsconfig

--- store/combat.ts
+++ store/combat.ts
@@ -6,14 +6,26 @@ export const useCombatStore = defineStore({
   id: "combat-store",
   state: () => {
     return {
-      classList: [],
+      active: false,
+      initiative: [], // ordered list of monsters and players
+      monstersList: [],
       playersList: [],
     };
   },
   actions: {
+    addMonster(monster) {
+      this.monstersList.push(monster);
+    },
     addPlayer(player) {
       this.playersList.push(player);
     },
+    /*
+      add monster to combat
+      start combat
+      end combat
+      deal damage
+      check if any are dead? unsure at this stage of the loop
+    */
   },
   getters: {
     players: (state) => state.playersList,
